cb2ef508549125e2111bd5d2241850c7757960fa
Maxim <68609996+squareGTS@users.noreply.github.com>
2021-01-07 16:43:47 +0300

    Initial Commit

---
+++ MaaskDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MaaskDemo
+//
+//  Created by Maxim Bekmetov on 07.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
