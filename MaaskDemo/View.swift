a186e62e3a2a68fdf9caec89512acc511c610877
Maxim <68609996+squareGTS@users.noreply.github.com>
2021-01-07 17:44:48 +0300

    Add button and view.

---
+++ MaaskDemo/View.swift
@@ -0,0 +1,33 @@
+//
+//  View.swift
+//  MaaskDemo
+//
+//  Created by Maxim Bekmetov on 07.01.2021.
+//
+
+import UIKit
+
+@IBDesignable
+class View: UIView {
+    
+    private var size = CGSize()
+    @IBInspectable var cornerRadiiSize: CGFloat = 0 {
+        didSet {
+            size = CGSize(width: cornerRadiiSize, height: cornerRadiiSize)
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: [.topLeft, .bottomRight], cornerRadii: size).cgPath
+        
+        layer.mask = shapeLayer
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        setNeedsLayout()
+    }
+}
+
